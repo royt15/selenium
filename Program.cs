5e921d6e5a1a05aa46b631027cfa14536c39af2a
okkop <okkop@invalid.com>
2025-02-04 14:34:43 +0200

    Add hello world and .gitignore

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        IWebDriver chromeDriver = new ChromeDriver();
+        string url = "https://www.google.fi";
+        chromeDriver.Navigate().GoToUrl(url);
+        string title = chromeDriver.Title;
+        Console.WriteLine($"{url} title is: ${title}");
+        Console.In.ReadLine();
+
+    }
+}
+
